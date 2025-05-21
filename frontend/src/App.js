1d35e3239ea61203fd5aec8351f2f534d3a77fd2
thamires33 <http.thami@gmail.com>
2025-05-21 15:40:34 -0300

    tela inicial da secretaria

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Login from './pages/Login';
 import Cadastro from './pages/Cadastro';
 import Home from './pages/Home';
 import Chamado from './pages/Chamado';
+import HomeSec from './pages/HomeSec';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/Cadastro" element={<Cadastro />} />
         <Route path="/Home" element={<Home />} />
         <Route path="/Chamado" element={<Chamado />} />
+        <Route path="/HomeSec" element={<HomeSec />}/>
       </Routes>
     </Router>
   );
