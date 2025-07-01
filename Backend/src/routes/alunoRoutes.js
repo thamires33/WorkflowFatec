63a0f222e9b5944509bb9260fe74a78948dbc8f2
Rafael Oliveira <rafael.oliveira.sss@gmail.com>
2025-07-01 02:15:39 -0300

    Ajustes no alunoController e alunoRoutes

--- Backend/src/routes/alunoRoutes.js
+++ Backend/src/routes/alunoRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const alunoController = require('../controllers/alunoController');
 
+// Cadastro de aluno
+router.post('/cadastrar', alunoController.cadastrar);
 
-router.post('/', alunoController.cadastrar);
+// Login de aluno
+router.post('/login', alunoController.login);
 
 module.exports = router;
